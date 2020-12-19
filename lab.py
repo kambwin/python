c2bd28efc7bb587dd0e875f48ef722b0e4c11c6f
kambwin <75625948+kambwin@users.noreply.github.com>
2020-12-19 21:33:18 +0600

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,44 @@
+class Character(object):
+	"""docstring for Character"""
+	def __init__(self, arg):
+		super(Character, self).__init__()
+		self.arg = arg
+		def attack(self):
+			self.attack = "2d6 + self.strenght"
+			return self.attribute
+
+		def save_throw(self, attribute):
+			self.attribute = "1d20 + self.dexterity"
+			return self.attribute
+
+	class Hero(object):
+		"""docstring for Hero"""
+		def __init__(self):
+			super(Hero, self).__init__()
+			def perk(self, name, level, power, dexterity, constitution, intellect, wisdom, charisma ):
+				self.name = Katana
+				self.level == 8
+				self.strenght == 18
+				self.dexterity == 12
+				self.constitution == 14
+				self.intellect == 8 
+				self.wisdom == 10
+				self.charisma == 8
+				return Hero
+
+	class dragon(object):
+		"""docstring for dragon"""
+		def __init__(self):
+			super(dragon, self).__init__()
+			def perk(self, name, level, strenght, dexterity, constitution, intellect, wisdom, charisma):
+					self.name = Gora
+					self.level = 10
+					self.strenght = 20
+					self.dexterity = 10
+					self.constitution = 16
+					self.intellect = 8
+					self.wisdom = 20
+					self.charisma = 8
+					return dragon
+		
+	
